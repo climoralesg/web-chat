0ac428ad6aea9589306e5569593f64c55d74aa42
climoralesg <38842608+climoralesg@users.noreply.github.com>
2020-07-27 23:30:52 -0400

    formulario register prueba

--- js/main.js
+++ js/main.js
@@ -26,17 +26,17 @@ $(document).ready(function () {
             const url = 'includes/userSearch.php';
             $.ajax({
                 url: url,
-                data:  postData ,
+                data: postData,
                 type: "POST",
                 contenttype: "application/json; charset=utf-8",
 
                 success: function (response) {
                     if (!response.error) {
-                        let respuesta=JSON.parse(response);
+                        let respuesta = JSON.parse(response);
                         console.log(respuesta);
                     }
                 },
-                error:function(){
+                error: function () {
                     console.log("Hubo un error en la consulta");
                 }
             });
@@ -47,9 +47,53 @@ $(document).ready(function () {
             */
 
 
-        // no fueron ingresados los valores de mail y pass
+            // no fueron ingresados los valores de mail y pass
         } else {
 
+        }
+    });
+
+
+    $('#register-form').submit(e => {
+        e.preventDefault(); //previene que actue a form y vaya al php
+        var check = true;
+        for (var i = 0; i < input.length; i++) {
+            if (validate(input[i]) == false) {
+                showValidate(input[i]);
+                check = false;
+            }
+        }
+        // Si fueron ingresados los valores de mail y pass correctamente
+        if (check == true) {
+
+            const postData = { //crea un objeto
+                email: $('#email').val(),
+                password: $('#password').val(),
+                userName: $('#userName').val()
+            };
+
+            console.log(postData);
+
+            const url = 'includes/userRegister.php';
+            
+            $.ajax({
+                url: url,
+                data: postData,
+                type:"POST",
+                contenttype: 'application/json; charset=utf-8',
+                success: function (response) {
+                    if (!response.error) {
+                        let respuesta = JSON.parse(response);
+                        console.log(respuesta);
+                    }
+                },
+                error: function () {
+                    console.log("hubo un error")
+                }
+            })
+            
+        } else {
+            // no fueron ingresados los valores de mail y pass
         }
 
     });
@@ -62,7 +106,7 @@ $(document).ready(function () {
     });
 
     function validate(input) {
-        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+        if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email' || $(input).attr('userName') == 'userName') {
             if ($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
                 return false;
             }
