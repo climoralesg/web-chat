0d2c4025249798a6affef2a0024571811e8ccf1b
climoralesg <38842608+climoralesg@users.noreply.github.com>
2020-08-01 21:56:39 -0400

    prueba realizada: aprobada

--- js/socketio.js
+++ js/socketio.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    const socket = io('http://localhost:3000');
+    const socket = io('http://localhost:4040');
     
     $(document).on('click', '#send', (e) => {
         //$("#output").append(`<p> <strong>Usuario</strong>:${$('#message').val()}</p>`);
@@ -7,8 +7,18 @@ $(document).ready(function () {
             message:$('#message').val(),
         });
     });
+
+    $('#message').keyup(function(){
+        socket.emit('chat:typing',"el usuario");
+        //socket.emit('chat:typing',userName.value);
+    })
+
     socket.on('chat:message',function(data){
-        $("#output").append(`<p> <strong>Usuario</strong>:${$('#message').val()}</p>`);
+        $("#output").append(`<p> <strong>Usuario:</strong>:${data.message}</p>`);
+    });
+
+    socket.on('chat:typing',function(data){
+        $("#actions").html(`<p><em>${data} Esta Escribiendo</em></p>`);
     })
 });
 
